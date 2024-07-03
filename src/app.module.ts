9b64305c5b361a58fd736999c54151c6ee2f5377
Alejandro Giraldo Ocampo <alejandrogiraldo1404@gmail.com>
2024-07-03 17:38:13 -0500

    Connecting database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { SubjectsModule } from './subjects/subjects.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule, SubjectsModule],
+  imports: [
+    TasksModule, 
+    SubjectsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123',
+      database: 'taskslistapp',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
