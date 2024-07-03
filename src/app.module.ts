4eab1b055ba05c3eec00a3bc2940b787ca96c2d0
Alejandro Giraldo Ocampo <alejandrogiraldo1404@gmail.com>
2024-07-03 17:29:20 -0500

    Adding Tasks and Subjects Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule, SubjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
